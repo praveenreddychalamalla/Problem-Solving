71d1e4d2390d1cb522a0224f4219ba19c3e37651
praveenreddychalamalla <praveenreddychalamalla@gmail.com>
2021-06-24 00:14:27 +0530

    first commit

---
+++ CPP/Arrays/Two Pointer Technique/container_with_most_water.cpp
@@ -0,0 +1,34 @@
+/**
+ * @author Praveen Reddy Chalamalla
+ * @create date 2021-06-22
+ * @desc Given n non-negative integers a_1, a_2, ..., a_n  where each represents a point at coordinate (i, a_i)             . ‘ n ‘ vertical lines are drawn such that the two endpoints of line i is at (i, a_i) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ */
+#include<bits/stdc++.h>
+using namespace std;
+int maxArea(int a[],int n) { //Time Complexity - O(n)
+   
+    int i=0,j=n-1,res=0; //Start with two pointers each pointing to left most and right most lines respectively, because the distance between them is maximum and check for better boundaries if any exist between them.
+    while(i<j){
+        int area=min(a[i],a[j])*(j-i); //Area between current boundaries
+        res=max(area,res);
+        //As we want to maximize the area, we keep the pointer pointing to the line which has more height as it is and move the other 
+        if(a[i]<a[j])i++; 
+        else j--;
+    }
+    return res;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) 
+            cin >> arr[i];
+        auto ans = maxArea(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+} 
---
+++ CPP/Arrays/maximum_subarray_sum.cpp
@@ -0,0 +1,31 @@
+/**
+ * @author Praveen Reddy Chalamalla
+ * @create date 2021-06-22
+ * @desc Maximum subarray sum - Kadanes Algorithm 
+ */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long;
+int maxSubArraySum(int a[],int n) { //Time Complexity - O(n)
+        int res=a[0],sum=a[0];
+        for(int i=0;i<n;i++){
+            sum=max(a[i],a[i]+sum);
+            res=max(res,sum);
+        }
+        return res;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) 
+            cin >> arr[i];
+        auto ans = maxSubArraySum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+} 
